e3b2fc3076126cc3781650e6f73861d923f03f20
stevensamir20 <steven.samir@eng.suez.edu.eg>
2024-01-21 16:32:47 +0200

    chat app

---
+++ Chat-Application/Structure.md
@@ -0,0 +1,15 @@
+# Frontend:
+
+## React Typescript
+
+### Chatscope UI Kit for React
+
+### Axios for Handling Requests
+
+### TanStack Query for caching and managing data
+
+### MUI for Any other Component/Styling
+
+# Backend and Database:
+
+## Firebase
---
+++ Chat-Application/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
